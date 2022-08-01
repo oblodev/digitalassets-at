82c7120d5202318d9bee12b651876e6150f62157
Dawid Kostka <dpkostka@gmail.com>
2022-08-01 12:57:34 +0200

    refactoring

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,8 +4,6 @@ export const ThemeContext = createContext();
 
 const themeReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE_COLOR":
-      return { ...state, color: action.payload };
     case "CHANGE_MODE":
       return { ...state, mode: action.payload };
     default:
@@ -15,20 +13,15 @@ const themeReducer = (state, action) => {
 
 export function ThemeProvider({ children }) {
   const [state, dispatch] = useReducer(themeReducer, {
-    color: "blue",
     mode: "light",
   });
-
-  const changeColor = (color) => {
-    dispatch({ type: "CHANGE_COLOR", payload: color });
-  };
 
   const changeMode = (mode) => {
     dispatch({ type: "CHANGE_MODE", payload: mode });
   };
 
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
+    <ThemeContext.Provider value={{ ...state, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
